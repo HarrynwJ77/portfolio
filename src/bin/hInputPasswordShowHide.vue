7b3a25a7de765de3ed76d3c8911b1241941f4b9f
psyhj1 <psyhj1@nottingham.ac.uk>
2023-12-15 01:54:18 +0000

    Changes: - Basic login input functionality implemented - Password show/hide button Todo: - Add functionality to cancel buttons - Add a login button on the main component

---
+++ src/bin/hInputPasswordShowHide.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { getImageUrl } from '@/services/commonGetImageUrl';
+import type { ForOfStatement } from 'typescript';
+import { ref } from 'vue';
+import hIconShow from './hIconShow.vue';
+import hIconHide from "./hIconHide.vue";
+
+function searchEmails() {
+  console.log("Searching emails: ")
+  /* TODO: On click, button should console log contents of input field */
+}
+
+var passwordShow = true;
+var buttonIcon = ref(getImageUrl("userInputPasswordShow.svg"));
+
+
+
+function showHide() {
+  var button = document.getElementById("btn")
+  var image = document.getElementById("btn-image");
+  if (passwordShow) {
+    image?.setAttribute("src", getImageUrl("userInputPasswordHide.svg"));
+    button?.setAttribute("title", "Hide Password");
+  } else {
+    image?.setAttribute("src", getImageUrl("userInputPasswordShow.svg"));
+    button?.setAttribute("title", "Hide Password");
+  }
+  passwordShow = ! passwordShow;
+}
+
+</script>
+
+<template>
+  <button id="btn" type="button" class="h-input-password-show-hide" @click="showHide()" title="Show password">
+    <img id="btn-image" :src="buttonIcon">
+  </button>
+</template>
+
+
+<style scoped>
+
+button {
+  all: inherit;
+  background: none;
+  border: none;
+  margin: none;
+  padding: 0px;
+  width: 20px;
+  height: 20px;
+}
+
+button:hover {
+  cursor:pointer;
+}
+
+button:active img{
+  content: src(getImageUrl("userInputPasswordHide.svg"))
+}
+
+</style>
--- src/components/hInputPasswordContainer.vue
+++ src/components/hInputPasswordContainer.vue
@@ -5,19 +5,73 @@ import { ref } from "vue";
 import hInputPasswordShowHide from "./hInputPasswordShowHide.vue";
 import hInputPasswordInput from "./hInputPasswordInput.vue";
 
+var passwordShow = true;
+var buttonIcon = ref(getImageUrl("userInputPasswordShow.svg"));
+
+
+
+function showHide() {
+  var input = document.getElementById("h-input-password");
+  var button = document.getElementById("h-btn-password-show-hide")
+  var image = document.getElementById("h-btn-password-show-hide-image");
+  if (passwordShow) {
+    input?.setAttribute("type", "text")
+    image?.setAttribute("src", getImageUrl("userInputPasswordHide.svg"));
+    button?.setAttribute("title", "Hide Password");
+  } else {
+    input?.setAttribute("type", "password")
+    image?.setAttribute("src", getImageUrl("userInputPasswordShow.svg"));
+    button?.setAttribute("title", "Hide Password");
+  }
+  passwordShow = ! passwordShow;
+}
+
 </script>
 <template>
     <div class="h-input-password-container">
         <label>Password</label>
         <form>
-            <h-input-password-input/>
-            <h-input-password-show-hide/>
+          <input id="h-input-password" type="password" class="h-input-password-input" autocomplete="on"/>
+          <button id="h-btn-password-show-hide" type="button" class="h-input-password-show-hide" @click="showHide()" title="Show password">
+            <img id="h-btn-password-show-hide-image" :src="buttonIcon">
+          </button>
         </form>
     </div>
 </template>
 <style scoped>
 
 
+button {
+  all: inherit;
+  background: none;
+  border: none;
+  margin: none;
+  padding: 0px;
+  width: 20px;
+  height: 20px;
+}
+
+button:hover {
+  cursor:pointer;
+}
+
+button:active img{
+  content: src(getImageUrl("userInputPasswordHide.svg"))
+}
+
+input {
+    background-color: inherit;
+    /* Tell the input to use all the available space */
+    flex-grow:2;
+    /* And hide the input's outline, so the form looks like the outline */
+    border:none;
+}
+
+
+/* remove the input focus blue box, it will be in the wrong place. */
+input:focus {
+  outline: none;
+}
 
 form {
   /* This bit sets up the horizontal layout */
